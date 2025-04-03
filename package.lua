e9a75e6d60a598b3b6e0bf5a80f2fc07c4cfc992
usk <75510861+mieusk@users.noreply.github.com>
2025-04-03 13:47:44 -0300

    Create package.lua

---
+++ package.lua
@@ -0,0 +1,10 @@
+return {
+  name = "mieusk/openai",
+  version = "1.0.0",
+  dependencies = {"coro-http", "json"},
+  description = "Now you can access OpenAI using Luvit",
+  tags = { "luvit", "openai" },
+  license = "MIT",
+  author = "mieusk",
+  homepage = "https://github.com/mieusk/openai"
+}
